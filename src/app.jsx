fe519d70e538ea4c1f697857b2344b22761d4f08
ray.lei <ray.lei@misde-MacBook-Pro-2.local>
2017-06-22 11:50:12 +0800

    1. Rewrite some codes.(store compose, import, export) 2. Add redux dev tool.

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,43 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import 'font-awesome/css/font-awesome.css';
-import './stylesheets/app.scss';
-import 'bootstrap/dist/js/bootstrap';
+// import 'bootstrap/dist/css/bootstrap.css';
+// import 'font-awesome/css/font-awesome.css';
+// import './stylesheets/app.scss';
+// import 'bootstrap/dist/js/bootstrap';
+require('bootstrap/dist/css/bootstrap.css');
+require('font-awesome/css/font-awesome.css');
+require('./stylesheets/app.scss');
+require('bootstrap/dist/js/bootstrap');
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/app';
 import {createStore, applyMiddleware, compose} from 'redux';
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
-import reducers from './reducers';
+// import reducers from './reducers';
+const reducers = require('./reducers');
 
-const store = compose(applyMiddleware(thunk))(createStore)(reducers);
+// const store = compose(applyMiddleware(thunk))(createStore)(reducers);
+// const store = applyMiddleware(thunk)(createStore)(reducers);
+// const store = createStore(
+//   reducers,
+//   {},
+//   compose(applyMiddleware(thunk))
+// );
+// const store = createStore(
+//   reducers,
+//   {},
+//   applyMiddleware(thunk)
+// );
+const store = createStore(
+  reducers,
+  {},
+  compose(
+    applyMiddleware(thunk),
+    // window.devToolsExtension ? window.devToolsExtension() : f => f
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
 ReactDOM.render(
 <Provider store={store}>
     <App />
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,17 @@
-import product from './product';
-import shopcart from './shopcart';
+// import product from './product';
+// import shopcart from './shopcart';
+import {default as product} from './product';
+import {default as shopcart} from './shopcart';
 
 import {combineReducers} from 'redux';
-export default combineReducers(
+
+// export default combineReducers(
+//     {
+//         productStore: product,
+//         shopcartStore: shopcart,
+//     }
+// );
+module.exports = combineReducers(
     {
         productStore: product,
         shopcartStore: shopcart,
